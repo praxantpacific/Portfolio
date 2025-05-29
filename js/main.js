e3bcbb3b3f9f229e95c10f6739ea4f8f8b9d1d51
praxantpacific <112964332+praxantpacific@users.noreply.github.com>
2025-05-29 12:03:45 +0545

    Update main.js

--- js/main.js
+++ js/main.js
@@ -40,6 +40,14 @@
 	// Scrollax
    $.Scrollax();
 
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 
 
    // Burger Menu
